c2439a1ffe4dbb5cfdb8b0c2365f5a92bfc13400
neuraX-admin <savardrjr@gmail.com>
2025-04-28 19:38:54 -0400

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -1,1 +1,17 @@
-git push
+const aedes = require('aedes')();
+const net = require('net');
+
+const server = net.createServer(aedes.handle);
+const port = 1883;
+
+server.listen(port, function () {
+  console.log('Serveur MQTT Aedes démarré sur le port', port);
+});
+
+aedes.on('client', function (client) {
+  console.log('Client connecté', client.id);
+});
+
+aedes.on('publish', function (packet, client) {
+  console.log('Message publié:', packet.payload.toString());
+});
