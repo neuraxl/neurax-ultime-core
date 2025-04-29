88547baae8cefbfe52cffe59363795ab40be3a63
neuraX-admin <savardrjr@gmail.com>
2025-04-28 22:30:20 -0400

    Create broker.js

---
+++ server/broker.js
@@ -0,0 +1,17 @@
+const aedes = require('aedes')();
+const net = require('net');
+
+const server = net.createServer(aedes.handle);
+const port = 1883;
+
+server.listen(port, function () {
+  console.log('Serveur MQTT Aedes démarré sur le port', port);
+});
+
+aedes.on('client', function (client) {
+  console.log('Client connecté', client?.id);
+});
+
+aedes.on('publish', function (packet, client) {
+  console.log('Message publié:', packet.payload.toString());
+});
