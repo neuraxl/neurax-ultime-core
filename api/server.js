4b1355c0830ff4d62adef10e35453300d1bac318
neuraX-admin <savardrjr@gmail.com>
2025-04-28 20:06:08 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.json());
+
+app.post('/neuron', (req, res) => {
+  console.log('Neuron data received:', req.body);
+  res.send('Neuron data received');
+});
+
+app.listen(port, () => {
+  console.log(API Server listening at http://localhost:${port});
+});
