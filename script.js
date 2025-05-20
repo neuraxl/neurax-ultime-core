3b050c3769bf9543abc994f94875d0797fa0b502
neuraX-admin <savardrjr@gmail.com>
2025-05-20 07:11:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function launchNeuraX() {
+  const phrase = "Connexion à NeuraX établie. Le système cérébral est opérationnel.";
+  const utterance = new SpeechSynthesisUtterance(phrase);
+  utterance.lang = "fr-FR";
+  speechSynthesis.speak(utterance);
+  generateGalaxies(5);
+  saveToMemory(phrase); // 🧠 enregistrement vocal
+}
 let neuronalMemory = [];
 
 function saveToMemory(data) {
